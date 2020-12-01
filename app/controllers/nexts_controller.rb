11f40cf4911e0ef1bc963cbefdf653df4309c1af
keiji-take <social.200512@gmial.com>
2020-12-01 09:04:04 +0900

    tweet投稿フォーム作成

--- app/controllers/nexts_controller.rb
+++ app/controllers/nexts_controller.rb
@@ -7,7 +7,16 @@ class NextsController < ApplicationController
     @tweet = Tweet.new
   end
   def create
-    
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :occupation, :boast)
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :occupation
+    validates :boast
+  end
 end
