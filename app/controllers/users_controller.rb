ee251e52bbbd92d636296757124622c92e83e17b
keiji-take <social.200512@gmial.com>
2020-12-19 07:52:33 +0900

    ユーザーコントローラーの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
+  before_action :user_set, only: [:show, :edit]
   def show
-    @user = User.find(params[:id])
     @tweets = @user.tweets
   end
   def edit
+  end
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :birth_date, :gender_id, :occupation_id, :tel_number, :location_id, :profile, :image)
+  end
+  def user_set
     @user = User.find(params[:id])
   end
 end
