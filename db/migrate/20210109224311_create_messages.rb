50841762c9bfb936c2c6e0b211aea9f0fa64f4fa
keiji-take <social.200512@gmial.com>
2021-01-25 14:40:40 +0900

    メッセージ時間　編集

--- db/migrate/20210109224311_create_messages.rb
+++ db/migrate/20210109224311_create_messages.rb
@@ -1,10 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.string :content,       null: false
-      t.references :room,      null: false
-      t.references :company,   null: false
-      t.references :user,      null: false
+      t.text :content,        null: false
+      t.references :room,     null: false
+      t.boolean :is_user,     null: false
+      t.boolean :is_company,  null: false
       t.timestamps
     end
   end
