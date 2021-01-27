c3a5c51f86bf21ed43f52f047e7062e399211566
keiji-take <social.200512@gmial.com>
2021-01-27 16:52:11 +0900

    検索機能追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -8,5 +8,11 @@ class Tweet < ApplicationRecord
   end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :occupation
-  
+  def self.search(search)
+    if search != ""
+      Tweet.where('boast LIKE(?)', "%#{search}%").or(Tweet.where('title LIKE(?)', "%#{search}%"))
+    else
+      Tweet.all
+    end
+  end
 end
